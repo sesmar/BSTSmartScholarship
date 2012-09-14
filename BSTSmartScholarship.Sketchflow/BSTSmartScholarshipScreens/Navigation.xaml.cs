18bc1238d9ade428b4167fb5bb3122feea021e58
Christopher M. Sims <sesmar@gmail.com>
2012-09-14 09:43:26 -0400

    Add sketchflow project for prototyping/wireframes

---
+++ BSTSmartScholarship.Sketchflow/BSTSmartScholarshipScreens/Navigation.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace BSTSmartScholarshipScreens
+{
+	public partial class Navigation : UserControl
+	{
+		public Navigation()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+	}
+}
---
+++ BSTSmartScholarship.Sketchflow/BSTSmartScholarshipScreens/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BSTSmartScholarship")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BSTSmartScholarship")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1758869c-246c-4c7c-ab89-28469b7a8f58")]
